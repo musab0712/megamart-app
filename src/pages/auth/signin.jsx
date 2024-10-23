95ad60dc07b4744b9ba516eeaac0579d07ce9f37
Musab Hassan <musabhassan1999@gmail.com>
2024-10-23 16:34:23 +0530

    signin and signup

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import CommonForm from "@/components/common/form";
+import { loginFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Signin() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function onSubmit() {}
   return (
-    <div>Signin</div>
-  )
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign In to your account
+        </h1>
+        <p className="mt-2">
+          Don't have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/signup"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 }
--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div> Signup </div>;
+  const [formData, setFormData] = useState({userName: "",
+    email: "",
+    password: "",
+  });
+
+  function onSubmit(){
+
+  }
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/signin"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+    formControls={registerFormControls}
+    buttonText={"Sign Up"}
+    formData={formData}
+    setFormData={setFormData}
+    onSubmit={onSubmit}
+  />
+    </div>
+  );
 }
