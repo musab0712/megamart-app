ce4f5c07910111cbd0d0852f5180100b634f1d57
Musab Hassan <musabhassan1999@gmail.com>
2024-12-09 15:11:27 +0530

    update home page

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,7 @@ import shopAddressSlice from "./shop/address-slice";
 import shopOrderSlice from "./shop/order-slice";
 import shopSearchSlice from "./shop/search-slice";
 import shopReviewSlice from "./shop/review-slice";
+import commonFeatureSlice from "./common-slice";
 
 const store = configureStore({
   reducer: {
@@ -19,6 +20,8 @@ const store = configureStore({
     shopOrder: shopOrderSlice,
     shopSearch: shopSearchSlice,
     shopReview: shopReviewSlice,
+
+    commonFeature: commonFeatureSlice,
   },
 });
 
